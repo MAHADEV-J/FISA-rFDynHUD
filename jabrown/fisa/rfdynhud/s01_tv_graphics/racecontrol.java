e598be951bcbcab8f523039bd3dcd5da0434a364
MAHADEV-J <j.a.browns.inbox@gmail.com>
2023-05-18 12:38:19 +0200

    more or less fixed font properties

--- jabrown/fisa/rfdynhud/s01_tv_graphics/racecontrol.java
+++ jabrown/fisa/rfdynhud/s01_tv_graphics/racecontrol.java
@@ -2,16 +2,21 @@ package jabrown.fisa.rfdynhud.s01_tv_graphics;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.io.IOException;
 
 import org.openmali.types.twodee.Rect2i;
 
 import jabrown.fisa.rfdynhud.s01_tv_graphics._util.JABrownFISAWidgetSets01_tv_graphics;
 import net.ctdp.rfdynhud.gamedata.LiveGameData;
+import net.ctdp.rfdynhud.properties.FontProperty;
+import net.ctdp.rfdynhud.properties.PropertiesContainer;
+import net.ctdp.rfdynhud.properties.PropertyLoader;
 import net.ctdp.rfdynhud.render.DrawnString;
 import net.ctdp.rfdynhud.render.DrawnString.Alignment;
 import net.ctdp.rfdynhud.render.DrawnStringFactory;
 import net.ctdp.rfdynhud.render.Texture2DCanvas;
 import net.ctdp.rfdynhud.render.TextureImage2D;
+import net.ctdp.rfdynhud.util.PropertyWriter;
 import net.ctdp.rfdynhud.util.SubTextureCollector;
 import net.ctdp.rfdynhud.valuemanagers.Clock;
 import net.ctdp.rfdynhud.widgets.base.widget.Widget;
@@ -28,6 +33,9 @@ public class racecontrol extends Widget
     private String scOut = "";
     private String scIn = "";
     private String redFlag = "";
+    private final FontProperty largeFont = new FontProperty("LargeFont", JABrownFISAWidgetSets01_tv_graphics.FISA_S01_TV_GRAPHICS_LARGE_FONT.getKey());
+    private final FontProperty captionFont = new FontProperty("CaptionFont", JABrownFISAWidgetSets01_tv_graphics.FISA_S01_TV_GRAPHICS_CAPTION_FONT.getKey());
+    private final FontProperty smallFont = new FontProperty("SmallFont", JABrownFISAWidgetSets01_tv_graphics.FISA_S01_TV_GRAPHICS_SMALL_FONT.getKey());
     
     public racecontrol()
     {
@@ -43,6 +51,50 @@ public class racecontrol extends Widget
         super.prepareForMenuItem();
         
         getFontProperty().setFont( "Dialog", Font.PLAIN, 9, false, true );
+        largeFont.setFont(JABrownFISAWidgetSets01_tv_graphics.FISA_S01_TV_GRAPHICS_LARGE_FONT_NAME, Font.BOLD, 64, true, true);
+        captionFont.setFont(JABrownFISAWidgetSets01_tv_graphics.FISA_S01_TV_GRAPHICS_CAPTION_FONT_NAME, Font.BOLD, 24, true, true);
+        smallFont.setFont(JABrownFISAWidgetSets01_tv_graphics.FISA_S01_TV_GRAPHICS_SMALL_FONT_NAME, Font.BOLD, 22, true, true);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void saveProperties( PropertyWriter writer ) throws IOException
+    {
+        super.saveProperties( writer );
+        
+        writer.writeProperty( largeFont, "Large font that is used for race control messages." );
+        writer.writeProperty( captionFont, "Font that is used for race control message caption." );
+        writer.writeProperty( smallFont, "Small font that is used for timing." );
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void loadProperty( PropertyLoader loader )
+    {
+        super.loadProperty( loader );
+        
+        if ( loader.loadProperty( largeFont ) );
+        else if ( loader.loadProperty( captionFont ) );
+        else if ( loader.loadProperty( smallFont ) );
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void getProperties( PropertiesContainer propsCont, boolean forceAll )
+    {
+        super.getProperties( propsCont, forceAll );
+        
+        propsCont.addGroup("Extra Properties");
+        
+        propsCont.addProperty(largeFont);
+        propsCont.addProperty(captionFont);
+        propsCont.addProperty(smallFont);
     }
     
     /**
@@ -56,8 +108,8 @@ public class racecontrol extends Widget
     @Override
     protected void initialize( LiveGameData gameData, boolean isEditorMode, DrawnStringFactory drawnStringFactory, TextureImage2D texture, int width, int height )
     {
-        dsCaption = drawnStringFactory.newDrawnString( "dsCaption", 240, 24, Alignment.LEFT, false, getFont(), isFontAntiAliased(), getFontColor() );
-    	dsInformation = drawnStringFactory.newDrawnString( "dsInformation", 240, 60, Alignment.LEFT, false, getFont(), isFontAntiAliased(), getFontColor() );
+        dsCaption = drawnStringFactory.newDrawnString( "dsCaption", 240, 24, Alignment.LEFT, false, captionFont.getFont(), isFontAntiAliased(), getFontColor() );
+    	dsInformation = drawnStringFactory.newDrawnString( "dsInformation", 240, 60, Alignment.LEFT, false, largeFont.getFont(), isFontAntiAliased(), getFontColor() );
     	captionText = "RACE CONTROL";
     	scOut = "SAFETY CAR";
     	scIn = "SAFETY CAR IN THIS LAP";
@@ -104,6 +156,14 @@ public class racecontrol extends Widget
     	textureCanvas.setColor(new Color(0, 0, 0, 80));
     	Rect2i rectangle = new Rect2i(offsetX, offsetY, width, height);
     	textureCanvas.fillRect(rectangle);
+    	
+    	textureCanvas.setColor(Color.YELLOW);
+    	int flagHeight = (5 * height) / 12;
+    	int flagWidth = (3 * flagHeight / 2);
+    	int flagOffsetX = offsetX + 28;
+    	int flagOffsetY = offsetY + 28;
+    	Rect2i flag = new Rect2i(flagOffsetX, flagOffsetY, flagWidth, flagHeight);
+    	textureCanvas.fillRect(flag);
     }
     
     @Override

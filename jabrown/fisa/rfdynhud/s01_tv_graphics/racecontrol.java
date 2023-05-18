906b66f8e17357fa6188061a104054f9b552e1f4
MAHADEV-J <j.a.browns.inbox@gmail.com>
2023-05-18 11:57:28 +0200

    added various messages and functions to toggle between them (not yet bound to key inputs); needs separate font sizes

--- jabrown/fisa/rfdynhud/s01_tv_graphics/racecontrol.java
+++ jabrown/fisa/rfdynhud/s01_tv_graphics/racecontrol.java
@@ -15,36 +15,22 @@ import net.ctdp.rfdynhud.render.TextureImage2D;
 import net.ctdp.rfdynhud.util.SubTextureCollector;
 import net.ctdp.rfdynhud.valuemanagers.Clock;
 import net.ctdp.rfdynhud.widgets.base.widget.Widget;
-import net.ctdp.rfdynhud.widgets.lessons._util.LessonsWidgetSet;
 
 /**
- * This Widget is something like the minimum, that you need to code to implement your first Widget.
- * 
- * As you can see, it is not much code, though it also doesn't do much.
- * 
- * This Widget will simply display the standard background and border with the text "Hello World".
- * 
- * @author Marvin Froehlich (CTDP)
+ * @author J.A. Brown
  */
 public class racecontrol extends Widget
 {
-    /*
-     * DrawnString objects are used to efficiently and easily draw texts on your Widget.
-     * They take care about clearing the exact area from the previous value, if the text
-     * needs to be redrawn (when it has changed).
-     * 
-     * The instance is usually created in the initialize() method implementation (see below).
-     */
-    private DrawnString ds = null;
+	private DrawnString dsCaption = null;
+    private DrawnString dsInformation = null;
+    private String captionText = "";
+    private String informationText = "";
+    private String scOut = "";
+    private String scIn = "";
+    private String redFlag = "";
     
     public racecontrol()
     {
-        /*
-         * In the constructor we pass in the default size of the Widget.
-         * We don't need to define a location, since this is done by the editor.
-         * In the below notation, we use percents, which is important
-         * to create proper sizes for all screen resolutions.
-         */
         super( JABrownFISAWidgetSets01_tv_graphics.INSTANCE, JABrownFISAWidgetSets01_tv_graphics.WIDGET_PACKAGE_S01_TV_GRAPHICS, 20.0f, 32.5f );
     }
     
@@ -70,29 +56,42 @@ public class racecontrol extends Widget
     @Override
     protected void initialize( LiveGameData gameData, boolean isEditorMode, DrawnStringFactory drawnStringFactory, TextureImage2D texture, int width, int height )
     {
-        /*
-         * This method is more or less only invoked once before the Widget is first rendered.
-         * 
-         * Ok, here we instantiate our DrawnString, that we will use to draw
-         * the "Hello World" text.
-         * 
-         * You cannot instantiate a DrawnString with 'new', but have to use the DrawnStringFactory passed in here.
-         * The reason is, that instances need to be cleaned up, when they are discarded. And this is done automatically
-         * by the factory.
-         * 
-         * Instances are identified by their name (first parameter), which is only used by the factory the clean up a replaced instance.
-         * So you can use any abbreviated string here. It just need to be the same, if you create a new instance, that replaces another one.
-         * 
-         * The string also need a position, which is to be understood relatively to the Widget (not absolute on the screen). We use top-left (0,0) here.
-         * 
-         * The alignment parameter tells the system to position the left end of the text at the given x-coordinate.
-         * 
-         * And we want to top edge of the text to be positioned at our y location (0), so we pass in a 'false' to tell it not to use the text's base line.
-         * 
-         * The other three parameters should be self explanatory.
-         * Their values come from some standard Poroperties, that any Widget has.
-         */
-        ds = drawnStringFactory.newDrawnString( "ds", 240, 60, Alignment.LEFT, false, getFont(), isFontAntiAliased(), getFontColor() );
+        dsCaption = drawnStringFactory.newDrawnString( "dsCaption", 240, 24, Alignment.LEFT, false, getFont(), isFontAntiAliased(), getFontColor() );
+    	dsInformation = drawnStringFactory.newDrawnString( "dsInformation", 240, 60, Alignment.LEFT, false, getFont(), isFontAntiAliased(), getFontColor() );
+    	captionText = "RACE CONTROL";
+    	scOut = "SAFETY CAR";
+    	scIn = "SAFETY CAR IN THIS LAP";
+    	redFlag = "RACE STOPPED";
+    	informationText = "NOT SPECIFIED";
+    }
+    
+    public void toggleInformationText(String text)
+    {
+    	informationText = text;
+    }
+    
+    public void toggleSafetyCarOut()
+    {
+    	if(informationText != scOut)
+    	{
+    		toggleInformationText(scOut);
+    	}
+    }
+    
+    public void toggleSafetyCarIn()
+    {
+    	if(informationText != scIn)
+    	{
+    		toggleInformationText(scIn);
+    	}
+    }
+    
+    public void toggleRedFlag()
+    {
+    	if(informationText != redFlag)
+    	{
+    		toggleInformationText(redFlag);
+    	}
     }
     
     @Override
@@ -103,40 +102,17 @@ public class racecontrol extends Widget
     	texture.clear(offsetX, offsetY, width, height, true, null);
     	Texture2DCanvas textureCanvas = texture.getTextureCanvas();
     	textureCanvas.setColor(new Color(0, 0, 0, 80));
-//    	textureCanvas.setAntialiazingEnabled(true);
-//    	final int triangleWidth = Math.round(height / 2f);
-//    	int[] xPoints = new int[] {offsetX, offsetX + triangleWidth, offsetX + triangleWidth, offsetX};
-//    	int[] yPoints = new int[] {offsetY + height, offsetY + height, offsetY, offsetY + height};
-//    	textureCanvas.fillPolygon(xPoints, yPoints, xPoints.length);
-//    	textureCanvas.setAntialiazingEnabled(false);
     	Rect2i rectangle = new Rect2i(offsetX, offsetY, width, height);
     	textureCanvas.fillRect(rectangle);
-//    	textureCanvas.setAntialiazingEnabled(true);
-//    	xPoints = new int[] {offsetX + width - triangleWidth, offsetX + width, offsetX + width - triangleWidth, offsetX + width - triangleWidth};
-//    	yPoints = new int[] {offsetY + height, offsetY, offsetY, offsetY + height};
-//    	textureCanvas.fillPolygon(xPoints, yPoints, xPoints.length);
     }
     
     @Override
     protected void drawWidget( Clock clock, boolean needsCompleteRedraw, LiveGameData gameData, boolean isEditorMode, TextureImage2D texture, int offsetX, int offsetY, int width, int height )
     {
-        /*
-         * Here we draw our Widget.
-         * 
-         * Since this method is called every time, the whole HUD is rerendered,
-         * we need to do something, so that our text is only drawn when it has actually changed.
-         * 
-         * Since we use a static text, we only need to ask, if the whole Widget is to be redrawn,
-         * which also needs our text to be drawn.
-         * 
-         * The DrawnString needs to know the (inner) Widget location to compute the actual text location
-         * from the widget relative location, that we defined up in the initialize() method
-         * and a target texture to draw on.
-         */
         if ( needsCompleteRedraw )
         {
-        	//texture.clear(new Color(255, 0, 0, 80), false, null);
-            ds.draw( offsetX, offsetY, "SAFETY CAR", texture );
+        	dsCaption.draw( offsetX, offsetY, captionText, texture );
+            dsInformation.draw( offsetX, offsetY, informationText, texture );
         }
     }
 }
